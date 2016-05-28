c15a30446fba5df7be1fd27370001bba1fcc8a24
sam <sam@163.com>
2016-05-28 21:55:22 +0800

    项目初始化建立

---
+++ stockshow/src/main/java/com/maoxf/stockshow/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.maoxf.stockshow.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 主界面
+ * Created by maoxiaofeng on 2016/5/28.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ stockshow/src/main/java/com/maoxf/stockshow/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.maoxf.stockshow.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.maoxf.stockshow.R;
+
+/**
+ * 引导界面
+ * Created by maoxiaofeng on 2016/5/28.
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
